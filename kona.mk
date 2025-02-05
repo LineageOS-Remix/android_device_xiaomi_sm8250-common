a285c5457e9052ef236bf38117c01fb4aeb077a7
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-05 19:12:48 +0800

    sm8250-common: Import Dolby C2 Media service
    from OnePlus 11 CPH2447_15.0.0.401(EX01)
    
    Change-Id: Icdfc5b11c7635d0707c2a39f54e0c8d149c771bc

--- kona.mk
+++ kona.mk
@@ -259,6 +259,7 @@ $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
+    media_codecs_c2_dolby_audio.xml \
     media_codecs_performance_c2.xml \
     video_system_specs.json
 
